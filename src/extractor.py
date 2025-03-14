5783a32c18c071734e93f86fd6aa5baa25ef5394
MagnusPC <103988876+MagnusPC@users.noreply.github.com>
2025-03-14 13:30:01 +0100

    eod

--- src/extractor.py
+++ src/extractor.py
@@ -22,13 +22,6 @@ print(amount_as_int, 'is', type(amount_as_int))
 amount_as_int = list(range(amount_as_int))
 print(amount_as_int[:10], "...")
 
-# prepare css-selector
-selector = "div.JUa6JJNj7R_Y3i4P8YUX:nth-child(2) > div:nth-child(2) > div:nth-child(#)"
-
-# prepare lists
-found_elements = []
-found_unique_elements = set()
-
 # get rid of popups to be able to send keys
 driver.maximize_window()
 driver.implicitly_wait(30)
@@ -48,13 +41,21 @@ interaction.context_click(an_element).perform()
 # prepare playlist view for scraping
 interaction.send_keys("\ue00f\ue00f").perform() # pagedown x2
 
+# prepare css-selector
+selector = "div.JUa6JJNj7R_Y3i4P8YUX:nth-child(2) > div:nth-child(2) > div:nth-child(#)"
+
+# prepare variables
+found_elements = []
+found_unique_elements = set()
 loaded_index = 0
+latest_element = None
 
 # loop through the playlist table rows
 for index, element in enumerate(amount_as_int, 1):
     # update the css-selector
-    if index > 39:
+    if index % 40 == 0:
         loaded_index = 1
+        interaction.send_keys("\ue00f\ue00f").perform() # pagedown x2
     else:
         loaded_index += 1
 
@@ -64,13 +65,17 @@ for index, element in enumerate(amount_as_int, 1):
     # get the webelement
     try:
         element = driver.find_element(By.CSS_SELECTOR, selector)
-        # element = driver.find_element(By.CLASS_NAME, 'j2s64Lz8y6VzBLB_V9Gm') # the button "play song by artist" class name
-    
+        # if class type = UpiE7J6vPrJIa59qxts4 (placeholder row) wait
+        isTemp = element.find_element(By.CLASS_NAME, "UpiE7J6vPrJIa59qxts4")
+
+        # print(element)
+        # print(isTemp)
+
         # add element to lists
         found_elements.append(element)
         found_unique_elements.add(element)
 
-        # site loads (and unloads) about 20 hits at a time, so we scroll to get more
+        # site loads (and unloads) about 23 hits at a time, so we scroll to get more
         # should be run 2 times before loop and then two times for each 20 hits
         # interaction.send_keys("\ue00c").perform() # escape key, escapes ad popup
         # interaction.send_keys("\ue00f\ue00f").perform() # PAGE_DOWN x2
@@ -84,12 +89,17 @@ for index, element in enumerate(amount_as_int, 1):
 
 
 driver.minimize_window()
+
 print(len(found_elements))
 print(len(found_unique_elements))
 
-# for webelement in found_elements:
-#     webelement = webelement.get_attribute('aria-label')
+print(found_elements.__getitem__(0).text) # as of 14.03.25, song 1
+print(found_elements.pop().text)
+print(found_unique_elements.pop().text) # as of 14.03.25, song 14 ?
 
-# print(found_elements[0])
+xset = set()
+for x in found_elements:
+    xset.add(x)
+    print(len(xset))
 
 driver.quit()
